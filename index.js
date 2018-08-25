b21d3833d14ba566e82c2cb3fcb0ef6600d6068a
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-25 19:42:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,5 +59,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    
+  }
+  else {
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
