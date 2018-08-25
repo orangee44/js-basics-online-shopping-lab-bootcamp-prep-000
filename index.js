49fa65379d7ce3b36c6e8cf8dd62703617ea04a4
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-25 19:25:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i=0; i < cart.length; i++){
-    if (item == cart[i].itemName){
+    if (cart[i].hasOwnProperty(item)){
       delete cart[i];
     }
     else {
