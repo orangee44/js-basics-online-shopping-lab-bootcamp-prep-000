29a1b31cd5f29579119f018ca960dc4611315fa7
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-25 19:07:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,30 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100) + 1;
+  var newObject = {itemName: item, itemPrice: price};
+  cart.push(newObject);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var len = cart.length;
+  if (len === 0) {
+    return `Your shopping cart is empty.`;
+  }
+  else {
+    var array = [];
+    if (len === 1) {
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+    }
+    else {
+      for (var i=0; i<(len - 1); i++) {
+      array.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`); 
+      }
+      array.push(` and ${cart[len -1].itemName} at $${cart[len - 1].itemPrice}`)
+      return `In your cart, you have${array}.`;
+    }
+  }
 }
 
 function total() {
